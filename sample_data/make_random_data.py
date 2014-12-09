e632d9c4db8a83e7d513ea38eaa901d5e6e8cf9d
Allen Park <allenpark@mit.edu>
2014-12-09 14:05:18 -0500

    made small data files

--- sample_data/make_random_data.py
+++ sample_data/make_random_data.py
@@ -4,17 +4,19 @@ import string
 
 c = string.printable[1:62]
 
-def gen_rand_cols(ncols, primary_key=True):
+def gen_rand_cols(ncols, primary_key):
     types = ['str', 'int', 'float', 'time']
     if primary_key:
         return ['id'] + [random.choice(types) for i in xrange(ncols-1)]
     else:
         return [random.choice(types) for i in xrange(ncols)]
 
-def gen_rand(i, t):
+def gen_rand(i, t, rand_errors):
     if t=='id':
         return str(i)
-    elif t=='str':
+    if random.random() < rand_errors:
+        return ''
+    if t=='str':
         return ''.join([random.choice(c) for i in xrange(random.randint(1, 1000))])
     elif t=='int':
         return str(random.randint(1, 1000000))
@@ -26,20 +28,20 @@ def gen_rand(i, t):
     else:
         raise Exception("Type " + str(t) + " is not in types.")
 
-def gen(filename, nrows, ncols, primary_key=True, write_header=True):
+def gen(filename, nrows, ncols, primary_key=True, write_header=True, rand_errors=0):
     with open(filename, 'w') as f:
-        cols = gen_rand_cols(ncols)
+        cols = gen_rand_cols(ncols, primary_key)
+        header = [cols[i] + str(i) for i in xrange(len(cols))]
         if write_header:
-            header = [cols[i] + str(i) for i in xrange(len(cols))]
-            print 'Header: ' + str(header)
-            raw_input()
             f.write(','.join(header) + '\n')
+        print 'Header: ' + str(header) + ' (Press Enter to confirm)'
+        raw_input()
         for i in xrange(nrows):
             if i % 1000 == 0:
                 print 'Generating row ' + str(i)
-            row = [gen_rand(i, t) for t in cols]
+            row = [gen_rand(i, t, rand_errors) for t in cols]
             f.write(','.join(row) + '\n')
         f.close()
 
 if __name__ == '__main__':
-    gen("dummy1.csv", 10000, 200)
+    gen("dummy1.csv", 100, 5, primary_key=True, write_header=True, rand_errors=0)
